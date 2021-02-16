6511a5cbcae140ee758df73f9d9631bafaf31dcd
arturo378 <amadrid378@hotmail.com>
2021-02-16 16:30:18 -0600

    Removed Firebase Token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,20 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyBq2I-AIoVQ-EPRqNpjkixayFrMRx4xp3I",
-    authDomain: "drive-in-93f2b.firebaseapp.com",
-    databaseURL: "https://drive-in-93f2b.firebaseio.com",
-    projectId: "drive-in-93f2b",
-    storageBucket: "",
-    messagingSenderId: "812838167312",
-    appId: "1:812838167312:web:3aedaa34ca8ddfe3e07b95",
-    measurementId: "G-JWY1PLNT8K"
-  },
-  
-};
+//Firebase Token Place
 
 /*
  * For easier debugging in development mode, you can import the following file
